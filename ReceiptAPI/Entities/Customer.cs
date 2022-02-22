36f1298a9c2f033ffc59d68d01fcdd774401731b
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-22 16:01:56 -0300

    Update Customer.cs

--- ReceiptAPI/Entities/Customer.cs
+++ ReceiptAPI/Entities/Customer.cs
@@ -5,7 +5,7 @@
         public int Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string HouseNumber { get; set; }
+        public int HouseNumber { get; set; }
         public string PostalCode { get; set; }
         public string Street { get; set; }
         public string District { get; set; }
