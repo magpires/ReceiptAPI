10a540a843e82a5f4be2044a16a32437a0b5b113
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-22 15:50:24 -0300

    Add Customer.cs

---
+++ ReceiptAPI/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace ReceiptAPI.Entities
+{
+    public class Customer : Base
+    {
+        public int Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
