fb2538f64be46bdd6576f15de3e0986506392a38
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-22 12:22:37 -0300

    Add ReceiptContext

---
+++ ReceiptAPI/Context/ReceiptContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceiptAPI.Entities;
+
+namespace ReceiptAPI.Context
+{
+    public class ReceiptContext : DbContext
+    {
+        public ReceiptContext(DbContextOptions<ReceiptContext> options) : base(options) { }
+
+        public DbSet<User> users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
