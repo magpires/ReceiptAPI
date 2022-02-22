535c829610bf61d6b06a2e30c2c2c0f557f3e15c
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-22 16:09:03 -0300

    Add CustomerMap.cs

---
+++ ReceiptAPI/Configurations/CustomerMap.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReceiptAPI.Entities;
+
+namespace ReceiptAPI.Configurations
+{
+    public class CustomerMap : BaseMap<Customer>
+    {
+        public CustomerMap() : base("customers") { }
+
+        public override void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(x => x.Email)
+                .HasColumnName("email")
+                .HasColumnType("varchar(100)");
+
+            builder.Property(x => x.PhoneNumber)
+                .HasColumnName("phone_number")
+                .HasColumnType("varchar(11)")
+                .IsRequired();
+
+            builder.Property(x => x.HouseNumber)
+                .HasColumnName("house_number")
+                .HasColumnType("integer")
+                .IsRequired();
+
+            builder.Property(x => x.PostalCode)
+                .HasColumnName("postal_code")
+                .HasColumnType("varchar(11)")
+                .IsRequired();
+
+            builder.Property(x => x.Street)
+                .HasColumnName("street")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(x => x.District)
+                .HasColumnName("district")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(x => x.City)
+                .HasColumnName("city")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(x => x.State)
+                .HasColumnName("state")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+        }
+    }
+}
