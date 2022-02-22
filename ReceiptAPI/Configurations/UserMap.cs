0685e7364b452b6c61a2fff415b8de28c0cdf9bf
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-22 15:29:18 -0300

    Add UserMap.cs

---
+++ ReceiptAPI/Configurations/UserMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReceiptAPI.Entities;
+
+namespace ReceiptAPI.Configurations
+{
+    public class UserMap : BaseMap<User>
+    {
+        public UserMap() : base("users") { }
+
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            base.Configure(builder);
+            builder.Property(x => x.Name).HasColumnName("name").HasColumnType("varchar(255)").IsRequired();
+            builder.Property(x => x.Email).HasColumnName("email").HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Password).HasColumnName("password").HasColumnType("varchar(255)").IsRequired();
+        }
+    }
+}
