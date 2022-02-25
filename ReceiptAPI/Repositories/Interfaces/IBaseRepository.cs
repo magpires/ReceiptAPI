1516fdf034f4d23fc47ef91722d6be6cd4dba74b
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-25 15:58:11 -0300

    Add IBaseRepository.cs

---
+++ ReceiptAPI/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ReceiptAPI.Repositories.Interfaces
+{
+    public interface IBaseRepository
+    {
+        public void Add<T>(T entity) where T : class;
+        public void Update<T>(T entity) where T : class;
+        public void Delete<T>(T entity) where T : class;
+        public Task<bool> SaveChangesAsync();
+    }
+}
