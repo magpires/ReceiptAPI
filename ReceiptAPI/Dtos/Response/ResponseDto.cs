0ffa1c62b88e8384e40311366ab15d65fb81f467
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-25 15:28:00 -0300

    Add ResponseDto.cs

---
+++ ReceiptAPI/Dtos/Response/ResponseDto.cs
@@ -0,0 +1,68 @@
+﻿using Flunt.Notifications;
+using System.Collections.Generic;
+
+namespace ReceiptAPI.Dtos.Response
+{
+    public class ResponseDto : Notifiable<Notification>
+    {
+        public int StatusCode { get; private set; }
+        public object Data { get; private set; }
+
+        public ResponseDto()
+        {
+            StatusCode = 200;
+        }
+
+        public ResponseDto(int statusCode)
+        {
+            StatusCode = statusCode;
+        }
+
+        public ResponseDto(int statusCode, object data)
+        {
+            StatusCode = statusCode;
+            Data = data;
+        }
+
+        public ResponseDto(int statusCode, Notifiable<Notification> contractNotifications)
+        {
+            StatusCode = statusCode;
+            AddNotifications(contractNotifications);
+        }
+
+        public ResponseDto(int statusCode, List<Notification> notifications)
+        {
+            StatusCode = statusCode;
+            notifications.ForEach(notification => AddNotification(notification));
+        }
+
+        public ResponseDto(int statusCode, Notifiable<Notification> contractNotifications, List<Notification> notifications)
+        {
+            StatusCode = statusCode;
+            notifications.ForEach(notification => AddNotification(notification));
+            AddNotifications(contractNotifications);
+        }
+
+        public ResponseDto(int statusCode, object data, Notifiable<Notification> contractNotifications)
+        {
+            StatusCode = statusCode;
+            Data = data;
+            AddNotifications(contractNotifications);
+        }
+
+        public ResponseDto(int statusCode, object data, List<Notification> notifications)
+        {
+            StatusCode = statusCode;
+            Data = data;
+            notifications.ForEach(notification => AddNotification(notification));
+        }
+
+        public ResponseDto(int statusCode, object data, Notifiable<Notification> contractNotifications, List<Notification> notifications)
+        {
+            StatusCode = statusCode;
+            Data = data;
+            notifications.ForEach(notification => AddNotification(notification));
+            AddNotifications(contractNotifications);
+        }
+    }
+}
