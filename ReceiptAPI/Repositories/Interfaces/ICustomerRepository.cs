d420d340c0e01905a437cf7c1d5a8181d248fe46
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-25 16:41:48 -0300

    Add ICustomerRepository.cs

---
+++ ReceiptAPI/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReceiptAPI.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Repositories.Interfaces
+{
+    public interface ICustomerRepository : IBaseRepository
+    {
+        Task<IEnumerable<Customer>> GetCustomersAsync();
+        Task<Customer> GetCustomerByIdAsync(int id);
+    }
+}
