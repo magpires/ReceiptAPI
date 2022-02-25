0674857f90de33be938be7f5c63913ecae665eef
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-25 17:03:19 -0300

    Add ICustomerService.cs

---
+++ ReceiptAPI/Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using ReceiptAPI.Dtos.Response;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Services.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<ResponseDto> GetCustomersAsync();
+        //Task<ResponseDto> GetCustomerByIdAsync(int id);
+        //Task<ResponseDto> PostCustomerAsync(CustomerPostDto customer);
+        //Task<ResponseDto> UpdateCustomerAsync(int id, CustomerUpdateDto customer);
+        //Task<ResponseDto> DeleteCustomerAsync(int id);
+    }
+}
