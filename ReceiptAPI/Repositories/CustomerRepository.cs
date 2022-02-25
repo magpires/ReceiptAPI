9fa8ef8dcf1f8389644a70536e6f5d665aeeeacf
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-25 16:41:58 -0300

    Add CustomerRepository.cs

---
+++ ReceiptAPI/Repositories/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceiptAPI.Context;
+using ReceiptAPI.Entities;
+using ReceiptAPI.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Repositories
+{
+    public class CustomerRepository : BaseRepository, ICustomerRepository
+    {
+        private readonly ReceiptContext _context;
+        public CustomerRepository(ReceiptContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
+    }
+}
