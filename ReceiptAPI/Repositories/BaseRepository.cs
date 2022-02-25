31ef87612920a41560bf139776929093bac5f18c
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-25 16:03:10 -0300

    Add BaseRepository.cs

---
+++ ReceiptAPI/Repositories/BaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using ReceiptAPI.Context;
+using ReceiptAPI.Repositories.Interfaces;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Repositories
+{
+    public class BaseRepository : IBaseRepository
+    {
+        private readonly ReceiptContext _context;
+
+        public BaseRepository(ReceiptContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
