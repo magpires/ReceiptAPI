daa1c5c77fc6fbb1868f44ef3af4b00045c58f8e
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-25 15:39:13 -0300

    Add CustomerController.cs

---
+++ ReceiptAPI/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return StatusCode(200, "Tá rodando");
+        }
+    }
+}
