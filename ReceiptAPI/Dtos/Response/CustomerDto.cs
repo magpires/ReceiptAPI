63cdbdcbe796b444fae67ccb6dcd1db9426b20ac
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-26 20:16:01 -0300

    Update CustomerDto.cs

--- ReceiptAPI/Dtos/Response/CustomerDto.cs
+++ ReceiptAPI/Dtos/Response/CustomerDto.cs
@@ -3,7 +3,7 @@
     public class CustomerDto
     {
         public int Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
     }
