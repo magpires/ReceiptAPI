01e165f0ebdfadf3d3d61cb3202872599da7c749
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-26 20:08:01 -0300

    Add ReceiptProfile.cs

---
+++ ReceiptAPI/Mappers/ReceiptProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReceiptAPI.Dtos.Response;
+using ReceiptAPI.Entities;
+
+namespace ReceiptAPI.Mappers
+{
+    public class ReceiptProfile : Profile
+    {
+        public ReceiptProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+        }
+    }
+}
