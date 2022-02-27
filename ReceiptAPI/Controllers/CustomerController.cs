608e7b144868b38d7189d4192d895afc4363122e
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-02-27 13:20:45 -0300

    Deploy

--- ReceiptAPI/Controllers/CustomerController.cs
+++ ReceiptAPI/Controllers/CustomerController.cs
@@ -15,6 +15,7 @@ namespace ReceiptAPI.Controllers
     {
         private readonly ICustomerService _service;
 
+        // Heroku
         public CustomerController(ICustomerService service)
         {
             _service = service;
