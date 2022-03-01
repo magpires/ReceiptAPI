5aa625dff96af57f10f9449835040c3a0e49938f
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-01 12:38:11 -0300

    Update Startup.cs

--- ReceiptAPI/Startup.cs
+++ ReceiptAPI/Startup.cs
@@ -31,7 +31,6 @@ namespace ReceiptAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers().AddNewtonsoftJson(options =>
             {
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
