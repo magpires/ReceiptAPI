85a8583e69096fc436944791c20aeca943aeac1a
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-01 17:42:22 -0300

    Update CustomerService.cs

--- ReceiptAPI/Services/CustomerService.cs
+++ ReceiptAPI/Services/CustomerService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Flunt.Notifications;
 using ReceiptAPI.Dtos.Response;
 using ReceiptAPI.Entities;
 using ReceiptAPI.Repositories.Interfaces;
@@ -18,7 +19,7 @@ namespace ReceiptAPI.Services
             _repository = repository;
             _mapper = mapper;
         }
-        
+
         public async Task<ResponseDto> GetCustomersAsync()
         {
             var customers = await _repository.GetCustomersAsync();
@@ -26,6 +27,35 @@ namespace ReceiptAPI.Services
             var customersDto =  _mapper.Map<List<CustomerDto>>(customers);
 
             return new ResponseDto(200, customersDto);
+        }
+
+        public async Task<ResponseDto> GetCustomerByIdAsync(int id)
+        {
+            List<Notification> notifications = new List<Notification>();
+
+            if (id <= 0)
+                notifications.Add(new Notification("id", "O id do cliente é inválido"));
+
+            var dataInvalid = notifications.Count > 0;
+
+            if (dataInvalid)
+                return new ResponseDto(400, notifications);
+
+            var customer = await _repository.GetCustomerByIdAsync(id);
+
+            var customerNotFound = customer == null;
+
+            if (customerNotFound)
+            {
+                notifications.Add(new Notification("data.customer", "Cliente não encontrado."));
+                return new ResponseDto(404, notifications); ;
+            }
+
+            var customersDto = _mapper.Map<CustomerDetailsDto>(customer);
+
+            return new ResponseDto(200, customersDto);
+
+            throw new System.NotImplementedException();
         }
     }
 }
