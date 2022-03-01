f1bccefbbfd0a7e15e06543bc6c3d34e7919f41e
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-01 17:42:08 -0300

    Update ICustomerService.cs

--- ReceiptAPI/Services/Interfaces/ICustomerService.cs
+++ ReceiptAPI/Services/Interfaces/ICustomerService.cs
@@ -6,7 +6,7 @@ namespace ReceiptAPI.Services.Interfaces
     public interface ICustomerService
     {
         Task<ResponseDto> GetCustomersAsync();
-        //Task<ResponseDto> GetCustomerByIdAsync(int id);
+        Task<ResponseDto> GetCustomerByIdAsync(int id);
         //Task<ResponseDto> PostCustomerAsync(CustomerPostDto customer);
         //Task<ResponseDto> UpdateCustomerAsync(int id, CustomerUpdateDto customer);
         //Task<ResponseDto> DeleteCustomerAsync(int id);
