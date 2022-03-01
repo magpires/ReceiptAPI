9d39461b055778bf2fb323a7142b142e3a0f8785
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-01 17:41:30 -0300

    Add CustomerDetailsDto.cs

---
+++ ReceiptAPI/Dtos/Response/CustomerDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ReceiptAPI.Dtos.Response
+{
+    public class CustomerDetailsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
