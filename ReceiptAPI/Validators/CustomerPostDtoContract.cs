57a097d4b5dc83584392f9ae244fc79df3642130
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-02 18:02:53 -0300

    Update CustomerPostDtosContract.cs

--- ReceiptAPI/Validators/CustomerPostDtoContract.cs
+++ ReceiptAPI/Validators/CustomerPostDtoContract.cs
@@ -5,22 +5,22 @@ namespace ReceiptAPI.Validators
 {
     public class CustomerPostDtoContract : Contract<CustomerPostDtoContract>
     {
-        public CustomerPostDtoContract(CustomerPostDto customerPostDto)
+        public CustomerPostDtoContract(CustomerPostDto customer)
         {
             Requires()
-                .IsNotNullOrEmpty(customerPostDto.Name, "data.nome", "O nome do cliente não pode ser nulo.")
-                .IsNotNullOrEmpty(customerPostDto.PhoneNumber, "data.phoneNumber", "O telefone do cliente não pode ser nulo.")
-                .IsLowerThan(customerPostDto.PhoneNumber, 12, "data.phoneNumber", "O telefone do cliente não deve ter mais que 11 digitos.")
-                .IsGreaterThan(customerPostDto.HouseNumber, 0, "data.houseNumber", "O número da casa não pode ser menor ou igual a 0.")
-                .IsNotNullOrEmpty(customerPostDto.PostalCode, "data.postalCode", "O CEP do cliente não pode ser nulo.")
-                .IsLowerThan(customerPostDto.PostalCode, 9, "data.postalCode", "O CEP do cliente não deve ter mais que 8 digitos.")
-                .IsNotNullOrEmpty(customerPostDto.District, "data.district", "O bairro do cliente não pode ser nulo.")
-                .IsNotNullOrEmpty(customerPostDto.City, "data.city", "A cidade do cliente não pode ser nula.")
-                .IsNotNullOrEmpty(customerPostDto.State, "data.state", "O Estado do cliente não pode ser nulo.");
+                .IsNotNullOrEmpty(customer.Name, "data.nome", "O nome do cliente não pode ser nulo.")
+                .IsNotNullOrEmpty(customer.PhoneNumber, "data.phoneNumber", "O telefone do cliente não pode ser nulo.")
+                .IsLowerThan(customer.PhoneNumber, 12, "data.phoneNumber", "O telefone do cliente não deve ter mais que 11 digitos.")
+                .IsGreaterThan(customer.HouseNumber, 0, "data.houseNumber", "O número da casa não pode ser menor ou igual a 0.")
+                .IsNotNullOrEmpty(customer.PostalCode, "data.postalCode", "O CEP do cliente não pode ser nulo.")
+                .IsLowerThan(customer.PostalCode, 9, "data.postalCode", "O CEP do cliente não deve ter mais que 8 digitos.")
+                .IsNotNullOrEmpty(customer.District, "data.district", "O bairro do cliente não pode ser nulo.")
+                .IsNotNullOrEmpty(customer.City, "data.city", "A cidade do cliente não pode ser nula.")
+                .IsNotNullOrEmpty(customer.State, "data.state", "O Estado do cliente não pode ser nulo.");
 
-            var emailNotNull = !string.IsNullOrEmpty(customerPostDto.Email);
+            var emailNotNullOrEmpty = !string.IsNullOrEmpty(customer.Email);
 
-            if (emailNotNull) IsEmail(customerPostDto.Email, "data.email", "Email inválido.");
+            if (emailNotNullOrEmpty) IsEmail(customer.Email, "data.email", "Email inválido.");
         }
     }
 }
