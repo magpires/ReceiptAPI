3baae039b08d8d0d3919c9cf5f824de9ded0b3c4
Weverton Pires <w.m.pires@hotmail.com>
2022-03-03 11:35:49 -0300

    Add Migrations

--- ReceiptAPI/Migrations/20220303143317_CreatAtAndUpdateAt.Designer.cs
+++ ReceiptAPI/Migrations/20220303143317_CreatAtAndUpdateAt.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReceiptAPI.Context;
 
 namespace ReceiptAPI.Migrations
 {
     [DbContext(typeof(ReceiptContext))]
-    partial class ReceiptContextModelSnapshot : ModelSnapshot
+    [Migration("20220303143317_CreatAtAndUpdateAt")]
+    partial class CreatAtAndUpdateAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
