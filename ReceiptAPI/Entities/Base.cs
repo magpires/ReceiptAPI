7ed58600ae9d5b4d8a988b57d92c2705a47008fc
Weverton Pires <w.m.pires@hotmail.com>
2022-03-03 11:35:34 -0300

    Update Base.cs

--- ReceiptAPI/Entities/Base.cs
+++ ReceiptAPI/Entities/Base.cs
@@ -1,7 +1,11 @@
-﻿namespace ReceiptAPI.Entities
+﻿using System;
+
+namespace ReceiptAPI.Entities
 {
     public class Base
     {
         public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
