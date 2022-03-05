393e55b04235a6b8145715f32047dbd57e4ee7e3
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-05 19:51:51 -0300

    Update UserDetailsDto.cs

---
+++ ReceiptAPI/Dtos/Response/UserDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReceiptAPI.Dtos.Response
+{
+    public class UserDetailsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
