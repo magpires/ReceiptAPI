1ebefe6df3d8465037983d06fae0360a90a0efb3
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-05 19:37:31 -0300

    Update User.cs

--- ReceiptAPI/Entities/User.cs
+++ ReceiptAPI/Entities/User.cs
@@ -2,7 +2,7 @@
 {
     public class User : Base
     {
-        public int Name { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
