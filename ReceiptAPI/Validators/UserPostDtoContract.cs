04f2e3cc98f91f37b28a27f57213db9532e9a14b
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-06 17:46:12 -0300

    Update UserPostDtoContract.cs

--- ReceiptAPI/Validators/UserPostDtoContract.cs
+++ ReceiptAPI/Validators/UserPostDtoContract.cs
@@ -8,16 +8,16 @@ namespace ReceiptAPI.Validators
         public UserPostDtoContract(UserPostDto user)
         {
             Requires()
-                .IsNotNullOrEmpty(user.Name, "data.name", "O nome do usuário não pode ser nulo.")
-                .IsLowerOrEqualsThan(user.Name, 255, "data.name", "O nome do cliente não deve ter mais que 255 digitos.")
-                .IsNotNullOrEmpty(user.Email, "data.email", "O email do usuário não pode ser nulo.")
-                .IsEmail(user.Email, "data.email", "Email inválido.")
-                .IsLowerThan(user.Email, 101, "data.email", "O email do usuário não pode ter mais que 100 digitos")
-                .IsNotNullOrEmpty(user.Password, "data.password", "A senha do usuário não pode ser nula.")
-                .IsLowerOrEqualsThan(user.Password, 256, "data.password", "A senha do usuário não deve ter mais que 255 digitos.")
-                .IsGreaterOrEqualsThan(user.Password, 8, "data.password", "A senha do usuário não deve ter menos que 8 digitos.")
-                .IsNotNullOrEmpty(user.ConfirmPassword, "data.confirmPassword", "A confirmação de senha não pode ser nula.")
-                .AreEquals(user.Password, user.ConfirmPassword, "data.password", "As senhas não coincidem.");
+                .IsNotNullOrEmpty(user.Name, "nameIsNotNullOrEmpty", "O nome do usuário não pode ser nulo.")
+                .IsLowerOrEqualsThan(user.Name, 255, "nameIsLowerOrEqualsThan", "O nome do cliente não deve ter mais que 255 digitos.")
+                .IsNotNullOrEmpty(user.Email, "emailIsNotNullOrEmpty", "O email do usuário não pode ser nulo.")
+                .IsEmail(user.Email, "emailIsEmail", "Email inválido.")
+                .IsLowerThan(user.Email, 101, "emailIsLowerThan", "O email do usuário não pode ter mais que 100 digitos")
+                .IsNotNullOrEmpty(user.Password, "passwordIsNotNullOrEmpty", "A senha do usuário não pode ser nula.")
+                .IsLowerOrEqualsThan(user.Password, 256, "passwordIsLowerOrEqualsThan", "A senha do usuário não deve ter mais que 255 digitos.")
+                .IsGreaterOrEqualsThan(user.Password, 8, "passwordIsGreaterOrEqualsThan", "A senha do usuário não deve ter menos que 8 digitos.")
+                .IsNotNullOrEmpty(user.ConfirmPassword, "confirmPasswordIsNotNullOrEmpty", "A confirmação de senha não pode ser nula.")
+                .AreEquals(user.Password, user.ConfirmPassword, "passwordAreEquals", "As senhas não coincidem.");
         }
     }
 }
