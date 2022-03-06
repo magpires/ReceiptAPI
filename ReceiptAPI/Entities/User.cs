dea163c33ef784e81bb2f267a8bfa38863dd7d58
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-06 13:02:24 -0300

    Update User.cs

--- ReceiptAPI/Entities/User.cs
+++ ReceiptAPI/Entities/User.cs
@@ -1,9 +1,14 @@
 ﻿namespace ReceiptAPI.Entities
 {
-    public class User : Base
+    public class User : BaseEntity
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public void EncryptPassword()
+        {
+            Password = BCrypt.Net.BCrypt.HashPassword(Password);
+        }
     }
 }
