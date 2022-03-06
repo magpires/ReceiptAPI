8e43edbd18f7d31ae0cb94ee8d0f01544a3d4efd
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-06 13:00:52 -0300

    Add BaseEntity,cs

---
+++ ReceiptAPI/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReceiptAPI.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public void SetCreatedAt()
+        {
+            CreatedAt = DateTime.Now;
+        }
+
+        public void SetUpdatedAt()
+        {
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
