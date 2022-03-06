a26b97ed8fb8f4410e475990d414278929e23009
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-06 13:00:33 -0300

    Add BaseEntityMap.cs

---
+++ ReceiptAPI/Configurations/BaseEntityMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReceiptAPI.Entities;
+
+namespace ReceiptAPI.Configurations
+{
+    public class BaseEntityMap<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        private readonly string _tableName;
+        public BaseEntityMap(string tableName)
+        {
+            _tableName = tableName;
+        }
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            if (!string.IsNullOrEmpty(_tableName)) builder.ToTable(_tableName);
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            builder.Property(x => x.CreatedAt).HasColumnName("created_at").IsRequired();
+            builder.Property(x => x.UpdatedAt).HasColumnName("updated_at");
+        }
+    }
+}
