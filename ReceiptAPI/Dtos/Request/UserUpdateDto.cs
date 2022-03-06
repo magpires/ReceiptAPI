f70bda1a03963d272fb93bf991b7102268e3b80a
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-06 20:37:01 -0300

    Add UserUpdateDto.cs

---
+++ ReceiptAPI/Dtos/Request/UserUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using ReceiptAPI.Validators;
+
+namespace ReceiptAPI.Dtos.Request
+{
+    public class UserUpdateDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public UserUpdateDtoContract Validate()
+        {
+            return new UserUpdateDtoContract(this);
+        }
+    }
+}
