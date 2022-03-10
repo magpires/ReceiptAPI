1bcf080251d350e1b5302dfa003d06b048c64cd7
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-10 20:03:37 -0300

    Add UserLoginPostDtoContract.cs

---
+++ ReceiptAPI/Validators/UserLoginPostDtoContract.cs
@@ -0,0 +1,17 @@
+﻿using Flunt.Validations;
+using ReceiptAPI.Dtos.Request;
+
+namespace ReceiptAPI.Validators
+{
+    public class UserLoginPostDtoContract : Contract<UserLoginPostDtoContract>
+    {
+        public UserLoginPostDtoContract(UserLoginPostDto user)
+        {
+            Requires()
+                .IsNotNullOrEmpty(user.Email, "emailIsNotNullOrEmpty", "Por favor, insira seu email.")
+                .IsEmail(user.Email, "emailIsEmail", "Email inválido.")
+                .IsNotNullOrEmpty(user.Password, "passwordIsNotNullOrEmpty", "Por favor, insira sua senha.")
+                .IsLowerOrEqualsThan(user.Password, 256, "passwordIsLowerOrEqualsThan", "A senha do usuário não deve ter mais que 255 digitos.");
+        }
+    }
+}
