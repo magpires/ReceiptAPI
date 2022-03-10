dda0f801ca34ed2144da2893552a2ed68b24ab49
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-10 20:03:16 -0300

    Add TokenService.cs

---
+++ ReceiptAPI/Services/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ReceiptAPI.Entities;
+using ReceiptAPI.Services.Interfaces;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ReceiptAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public string GenerateToken(User user)
+        {
+            var tokenHandle = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("TokenSettings:Secret").Value);
+            var expires = Convert.ToInt32(_configuration.GetSection("TokenSettings:ExpiresTokenInHours").Value);
+
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Role, "Admin")
+                }),
+
+                Expires = DateTime.UtcNow.AddHours(expires),
+                SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+
+            var token = tokenHandle.CreateToken(tokenDescription);
+
+            return tokenHandle.WriteToken(token);
+        }
+    }
+}
