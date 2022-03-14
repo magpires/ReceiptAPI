82838630fee867f9a5a0bbf069b954cd2fa8a8ec
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-14 20:38:28 -0300

    Add ReceiptMap.cs

---
+++ ReceiptAPI/Configurations/ReceiptMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReceiptAPI.Entities;
+
+namespace ReceiptAPI.Configurations
+{
+    public class ReceiptMap : BaseEntityMap<Receipt>
+    {
+        public ReceiptMap() : base("receipt") { }
+
+        public override void Configure(EntityTypeBuilder<Receipt> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.CustomerId)
+                .HasColumnName("customer_id")
+                .IsRequired();
+
+            builder.Property(x => x.PaymentMethod)
+                .HasColumnName("payment_method");
+        }
+    }
+}
