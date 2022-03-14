8cb8cc1d1fd11c7b5a4f1192eaf06df1c51bff96
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-14 20:38:14 -0300

    Add ProductReceiptMap.cs

---
+++ ReceiptAPI/Configurations/ProductReceiptMap.cs
@@ -0,0 +1,26 @@
+﻿using ReceiptAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Consultorio.Map
+{
+    public class ProductReceiptMap : IEntityTypeConfiguration<ProductReceipt>
+    {
+        public virtual void Configure(EntityTypeBuilder<ProductReceipt> modelBuilder)
+        {
+            modelBuilder.ToTable("product_receipt");
+            modelBuilder.HasKey(pr => pr.Id);
+            modelBuilder.Property(pr => pr.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            modelBuilder.Property(pr => pr.ProductId).HasColumnName("product_id").IsRequired();
+            modelBuilder.Property(pr => pr.ReceiptId).HasColumnName("receipt_id").IsRequired();
+
+            modelBuilder.HasOne(pr => pr.Product)
+                .WithMany()
+                .HasForeignKey(pr => pr.ProductId);
+
+            modelBuilder.HasOne(pr => pr.Receipt)
+                .WithMany()
+                .HasForeignKey(pr => pr.ReceiptId);
+        }
+    }
+}
