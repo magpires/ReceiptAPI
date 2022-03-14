2d7ca3985fd62a41e8401041feffaa8331e81837
Weverton Pires <w.m.pires@hotmail.com>
2022-03-14 16:03:48 -0300

    Add ProductReceipt.cs

---
+++ ReceiptAPI/Entities/ProductReceipt.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReceiptAPI.Entities
+{
+    public class ProductReceipt
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int ReceiptId { get; set; }
+        public Receipt Receipt { get; set; }
+    }
+}
