29f2c6b3b828dd031c63c77966e614608b75f2a0
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-15 13:32:31 -0300

    Add ReceiptRepository.cs and IReceiptRepository.cs

---
+++ ReceiptAPI/Repositories/Interfaces/IReceiptRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using ReceiptAPI.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Repositories.Interfaces
+{
+    public interface IReceiptRepository : IBaseRepository
+    {
+        Task<IEnumerable<Receipt>> GetReceiptsAsync();
+        Task<IEnumerable<Receipt>> GetReceiptsByCustomerIdAsync(int customerId);
+        Task<Receipt> GetReceiptByIdAsync(int id);
+    }
+}
---
+++ ReceiptAPI/Repositories/ReceiptRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceiptAPI.Context;
+using ReceiptAPI.Entities;
+using ReceiptAPI.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Repositories
+{
+    public class ReceiptRepository : BaseRepository, IReceiptRepository
+    {
+        private readonly ReceiptContext _context;
+        public ReceiptRepository(ReceiptContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Receipt>> GetReceiptsAsync()
+        {
+            return await _context.Set<Receipt>()
+                .Include(x => x.Customer)
+                .Include(x => x.Products)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Receipt>> GetReceiptsByCustomerIdAsync(int customerId)
+        {
+            return await _context.Set<Receipt>()
+                .Include(x => x.Customer)
+                .Include(x => x.Products)
+                .Where(x => x.CustomerId == customerId)
+                .ToListAsync();
+        }
+
+        public async Task<Receipt> GetReceiptByIdAsync(int id)
+        {
+            return await _context.Set<Receipt>()
+                .Include(x => x.Customer)
+                .Include(x => x.Products)
+                .Where(x =>x.Id == id)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
