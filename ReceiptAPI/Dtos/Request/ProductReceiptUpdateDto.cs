7315ab2059f9a597028dc8a6953446f2dfb339b7
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-20 21:08:47 -0300

    Add Requests ReceiptsDtos

---
+++ ReceiptAPI/Dtos/Request/ProductReceiptUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using ReceiptAPI.Enumerators;
+using ReceiptAPI.Validators;
+using System.Collections.Generic;
+
+namespace ReceiptAPI.Dtos.Request
+{
+    public class ProductReceiptUpdateDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        public ProductReceiptUpdateDtoContract Validate()
+        {
+            return new ProductReceiptUpdateDtoContract(this);
+        }
+    }
+}
---
+++ ReceiptAPI/Dtos/Request/ReceiptPostDto.cs
@@ -0,0 +1,18 @@
+﻿using ReceiptAPI.Enumerators;
+using ReceiptAPI.Validators;
+using System.Collections.Generic;
+
+namespace ReceiptAPI.Dtos.Request
+{
+    public class ReceiptPostDto
+    {
+        public int CustomerId { get; set; }
+        public List<ProductReceiptPostDto> ProductReceipts { get; set; }
+        public int PaymentMethod { get; set; }
+
+        public ReceiptPostDtoContract Validate()
+        {
+            return new ReceiptPostDtoContract(this);
+        }
+    }
+}
