2f656ba66a837977e80fb4c3aa01e3592fc7cf18
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-20 21:10:48 -0300

    Update ProductReceipt.cs

--- ReceiptAPI/Entities/ProductReceipt.cs
+++ ReceiptAPI/Entities/ProductReceipt.cs
@@ -4,10 +4,10 @@ namespace ReceiptAPI.Entities
 {
     public class ProductReceipt
     {
-        public int Id { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
         public int ReceiptId { get; set; }
         public Receipt Receipt { get; set; }
+        public int Quantity { get; set; }
     }
 }
