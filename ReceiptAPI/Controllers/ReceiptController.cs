96c6e0269c89c690176c9ed8810c72b62a6f17bb
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-20 21:07:52 -0300

    Add ReceiptController.cs

---
+++ ReceiptAPI/Controllers/ReceiptController.cs
@@ -0,0 +1,70 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReceiptAPI.Dtos.Request;
+using ReceiptAPI.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class ReceiptController : ControllerBase
+    {
+        private readonly IReceiptService _service;
+
+        public ReceiptController(IReceiptService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var response = await _service.GetReceiptsAsync();
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("/Customer/{customerId}")]
+        public async Task<IActionResult> GetByCustomerIdAsync(int customerId)
+        {
+            var response = await _service.GetReceiptsByCustomerIdAsync(customerId);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var response = await _service.GetReceiptByIdAsync(id);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(ReceiptPostDto receipt)
+        {
+            var response = await _service.PostReceiptAsync(receipt);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAsync(int id, ReceiptUpdateDto receipt)
+        {
+            var response = await _service.UpdateReceiptAsync(id,receipt);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var response = await _service.DeleteReceiptAsync(id);
+
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
