63e28e18ba0bc9f2ecc3e43fa7e9067ba30d8f60
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-20 21:15:49 -0300

    Add ReceiptService

---
+++ ReceiptAPI/Services/Interfaces/IReceiptService.cs
@@ -0,0 +1,17 @@
+﻿using ReceiptAPI.Dtos.Request;
+using ReceiptAPI.Dtos.Response;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Services.Interfaces
+{
+    public interface IReceiptService
+    {
+        Task<ResponseDto> GetReceiptsAsync();
+        Task<ResponseDto> GetReceiptsByCustomerIdAsync(int customerId);
+        Task<ResponseDto> GetReceiptByIdAsync(int id);
+        Task<ResponseDto> PostReceiptAsync(ReceiptPostDto receipt);
+        Task<ResponseDto> UpdateReceiptAsync(int id, ReceiptUpdateDto receipt);
+        Task<ResponseDto> DeleteReceiptAsync(int id);
+    }
+}
+ 
