ff34b27760585bdb886e64b7e8da1a9cfc0f7477
Weverton Pires <w.m.pires@hotmail.com>
2022-03-22 16:25:27 -0300

    Update UserCreateDtoContract.cs

--- ReceiptAPI/Validators/UserCreateDtoContract.cs
+++ ReceiptAPI/Validators/UserCreateDtoContract.cs
@@ -12,9 +12,9 @@ namespace ReceiptAPI.Validators
                 .IsLowerOrEqualsThan(user.Name, 255, "nameIsLowerOrEqualsThan", "O nome do usuário não deve ter mais que 255 digitos.")
                 .IsNotNullOrEmpty(user.Email, "emailIsNotNullOrEmpty", "O email do usuário não pode ser nulo.")
                 .IsEmail(user.Email, "emailIsEmail", "Email inválido.")
-                .IsLowerThan(user.Email, 101, "emailIsLowerThan", "O email do usuário não pode ter mais que 100 digitos")
+                .IsLowerThan(user.Email, 100, "emailIsLowerThan", "O email do usuário não pode ter mais que 100 digitos")
                 .IsNotNullOrEmpty(user.Password, "passwordIsNotNullOrEmpty", "A senha do usuário não pode ser nula.")
-                .IsLowerOrEqualsThan(user.Password, 256, "passwordIsLowerOrEqualsThan", "A senha do usuário não deve ter mais que 255 digitos.")
+                .IsLowerOrEqualsThan(user.Password, 255, "passwordIsLowerOrEqualsThan", "A senha do usuário não deve ter mais que 255 digitos.")
                 .IsGreaterOrEqualsThan(user.Password, 8, "passwordIsGreaterOrEqualsThan", "A senha do usuário não deve ter menos que 8 digitos.")
                 .IsNotNullOrEmpty(user.ConfirmPassword, "confirmPasswordIsNotNullOrEmpty", "A confirmação de senha não pode ser nula.")
                 .AreEquals(user.Password, user.ConfirmPassword, "passwordAreEquals", "As senhas não coincidem.");
