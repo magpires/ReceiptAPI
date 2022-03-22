e75e6e96fda0f18f6e2f125b62edc69ba83e111a
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-22 00:31:35 -0300

    Add ResponseDtos

---
+++ ReceiptClient/Dtos/Response/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptClient.Dtos.Response
+{
+    public class ResponseDto
+    {
+        public int statusCode { get; private set; }
+        public object? data { get; private set; }
+    }
+}
