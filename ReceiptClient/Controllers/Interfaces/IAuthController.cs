b6060d647eaa5cb83ab4e72f6dd19e3c59f163b5
Weverton Pires <w.m.pires@hotmail.com>
2022-03-22 15:44:04 -0300

    Add IAuthController,cs

---
+++ ReceiptClient/Controllers/Interfaces/IAuthController.cs
@@ -0,0 +1,15 @@
+﻿using ReceiptClient.Dtos.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptClient.Controllers.Interfaces
+{
+    public interface IAuthController
+    {
+        Task<HttpResponseMessage> Login(UserLoginPostDto user);
+    }
+}
