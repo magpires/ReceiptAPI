ded7313f04a4f47f9591a034132f66062a2e1b54
Weverton Pires <w.m.pires@hotmail.com>
2022-03-22 14:33:53 -0300

    Update AuthController.cs

--- ReceiptClient/Controllers/AuthController.cs
+++ ReceiptClient/Controllers/AuthController.cs
@@ -2,9 +2,8 @@
 using Newtonsoft.Json.Linq;
 using ReceiptClient.Dtos.Request;
 using ReceiptClient.Dtos.Response;
-using System;
+using ReceiptClient.Views;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -15,7 +14,7 @@ namespace ReceiptClient.Controllers
 {
     public class AuthController
     {
-        public static async void Login(UserLoginPostDto user)
+        public async static Task<HttpResponseMessage> Login(UserLoginPostDto user)
         {
             string URI = "https://receiptapi22.herokuapp.com/api/Auth/Login";
 
@@ -24,10 +23,7 @@ namespace ReceiptClient.Controllers
                 var serializedLogin = JsonConvert.SerializeObject(user);
                 var content = new StringContent(serializedLogin, Encoding.UTF8, "application/json");
                 var result = await client.PostAsync(URI, content);
-
-                var jsonString = await result.Content.ReadAsStringAsync();
-                var tokenData = JObject.Parse(jsonString);
-                string token = (string)tokenData.SelectToken("data.token");
+                return result;
             }
         }
     }
