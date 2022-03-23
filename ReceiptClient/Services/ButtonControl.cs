445eb5bb32252ce8df709610e4823ce6ebf6b155
Weverton Pires <w.m.pires@hotmail.com>
2022-03-23 15:57:32 -0300

    Add ButtonEnableControl.cs

---
+++ ReceiptClient/Services/ButtonControl.cs
@@ -0,0 +1,26 @@
+﻿using ReceiptClient.Services.Interfaces;
+using System;
+using System.Windows.Forms;
+
+namespace ReceiptClient.Services
+{
+    public class ButtonEnableControl : IButtonEnableControl
+    {
+        public bool CheckTexBoxtFilled(Control.ControlCollection controls)
+        {
+            foreach (Control c in controls)
+            {
+                if (c is TextBox && c.Visible)
+                {
+                    if (String.IsNullOrEmpty(((TextBox)c).Text))
+                        return false;
+                }
+
+                if (c.HasChildren)
+                    return CheckTexBoxtFilled(c.Controls);
+            }
+
+            return true;
+        }
+    }
+}
