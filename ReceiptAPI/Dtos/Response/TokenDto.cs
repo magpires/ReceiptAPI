b224a234f0b841ccbad22c7ff1039604cfdee2a9
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-23 22:00:59 -0300

    Update TokenDto.cs

--- ReceiptAPI/Dtos/Response/TokenDto.cs
+++ ReceiptAPI/Dtos/Response/TokenDto.cs
@@ -1,8 +1,11 @@
-﻿namespace ReceiptAPI.Dtos.Response
+﻿using System;
+
+namespace ReceiptAPI.Dtos.Response
 {
     public class TokenDto
     {
         public UserDetailsDto User { get; set; }
         public string Token { get; set; }
+        public DateTime ExpirationDate { get; set; }
     }
 }
