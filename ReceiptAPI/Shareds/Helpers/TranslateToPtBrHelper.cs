2c6273c72e61889f7cd9100133b28547b0e1faf6
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-29 20:32:48 -0300

    Add TranslateToPtBrHelper.cs

---
+++ ReceiptAPI/Shareds/Helpers/TranslateToPtBrHelper.cs
@@ -0,0 +1,21 @@
+﻿using ReceiptAPI.Enumerators;
+
+namespace ReceiptAPI.Shared.Helpers
+{
+    public static class TranslateToPtBrHelper
+    {
+        public static string TranslatePaymentMethod(PaymentMethod paymentMethod)
+        {
+            if (paymentMethod == PaymentMethod.CASH)
+            {
+                return "Dinheiro";
+            }
+
+            if (paymentMethod == PaymentMethod.CREDITCARD)
+            {
+                return "Crédito";
+            }
+            return "Método de pagamento desconhecido";
+        }
+    }
+}
