392a462d2e4926f6fc5414454698eb6c73996c71
Weverton Pires <30535993+magpires@users.noreply.github.com>
2022-03-31 18:15:04 -0300

    Update ReceiptHelper

---
+++ ReceiptAPI/Shareds/Helpers/ReceiptHelper.cs
@@ -0,0 +1,21 @@
+﻿using ReceiptAPI.Dtos.Response;
+using ReceiptAPI.Enumerators;
+using System.Collections.Generic;
+
+namespace ReceiptAPI.Shared.Helpers
+{
+    public static class ReceiptHelper
+    {
+        public static double CalculateTotalReceiptPrice(List<ProductReceiptDetailsDto> products)
+        {
+            double total = 0;
+
+            products.ForEach(p =>
+            {
+                total += p.Total;
+            });
+
+            return total;
+        }
+    }
+}
