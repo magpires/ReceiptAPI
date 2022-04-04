66d384211afcf585e0d4985e8569f9238cf9ec46
Weverton Pires <w.m.pires@hotmail.com>
2022-04-04 16:30:46 -0300

    Add EnumController.cs

---
+++ ReceiptAPI/Controllers/EnumController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReceiptAPI.Dtos.Request;
+using ReceiptAPI.Services;
+using ReceiptAPI.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //[Authorize(AuthenticationSchemes = "Bearer")]
+    public class EnumController : ControllerBase
+    {
+
+        public EnumController()
+        {
+        }
+
+        [HttpGet("PaymentsMethods")]
+        public IActionResult GetPaymentsMethods()
+        {
+            var response = EnumService.GetPaymentsMethods();
+
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
