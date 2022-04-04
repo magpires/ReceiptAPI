8302dc36d63f7a5e79fd016e42621757666fe955
Weverton Pires <w.m.pires@hotmail.com>
2022-04-04 16:31:18 -0300

    Add EnumService.cs

---
+++ ReceiptAPI/Services/EnumService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Flunt.Notifications;
+using ReceiptAPI.Dtos.Request;
+using ReceiptAPI.Dtos.Response;
+using ReceiptAPI.Entities;
+using ReceiptAPI.Enumerators;
+using ReceiptAPI.Repositories.Interfaces;
+using ReceiptAPI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReceiptAPI.Services
+{
+    public class EnumService
+    {
+        private static IMapper _mapper;
+
+        public EnumService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public static ResponseDto GetPaymentsMethods()
+        {
+            var enums = Enum.GetValues<PaymentMethod>().Cast<PaymentMethod>()
+                            .Select(v => v.ToString());
+
+            var enumInt = Enum.GetValues<PaymentMethod>().Cast<PaymentMethod>();
+
+            return new ResponseDto(200, enumInt);
+        }
+    }
+}
