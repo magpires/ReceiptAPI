ef1ba4559494dcd1e17a0e4d4d42301fc175d3f8
Weverton Pires <w.m.pires@hotmail.com>
2022-04-13 15:11:23 -0300

    Update CustomerUpdateDto.cs

--- ReceiptAPI/Dtos/Request/CustomerUpdateDto.cs
+++ ReceiptAPI/Dtos/Request/CustomerUpdateDto.cs
@@ -7,7 +7,7 @@ namespace ReceiptAPI.Dtos.Request
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public int HouseNumber { get; set; }
+        public int? HouseNumber { get; set; }
         public string PostalCode { get; set; }
         public string Street { get; set; }
         public string District { get; set; }
